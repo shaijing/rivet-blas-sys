bd863d37d808efbe0dc5c16d279b5d2fc062d766
shaijing <lingyu.lynn@outlook.com>
2026-09-20 14:48:07 +0800

    test: cover backend-specific CBLAS extensions

---
+++ tests/test_mkl_extensions.rs
@@ -0,0 +1,44 @@
+#![cfg(feature = "intel-mkl")]
+
+use rivet_blas_sys::cblas::cblas_level_one_mkl::cblas_isamin;
+use rivet_blas_sys::cblas::cblas_level_three_mkl::cblas_strmm_oop;
+use rivet_blas_sys::cblas::cblas_types::{
+    CBlasDiag, CBlasLayout, CBlasSide, CBlasTranspose, CBlasUplo,
+};
+
+#[test]
+fn mkl_ordinary_extension_supports_iamin() {
+    let x = [3.0_f32, -1.0, 2.0];
+    let index = unsafe { cblas_isamin(3, x.as_ptr(), 1) };
+
+    assert_eq!(index, 1);
+}
+
+#[test]
+fn mkl_trmm_oop_writes_to_a_separate_matrix() {
+    let a = [2.0_f32, 0.0, 1.0, 3.0];
+    let b = [1.0_f32, 2.0, 3.0, 4.0];
+    let mut c = [0.0_f32; 4];
+
+    unsafe {
+        cblas_strmm_oop(
+            CBlasLayout::CBlasRowMajor,
+            CBlasSide::CblasLeft,
+            CBlasUplo::CblasLower,
+            CBlasTranspose::CBlasNoTrans,
+            CBlasDiag::CblasNonUnit,
+            2,
+            2,
+            1.0,
+            a.as_ptr(),
+            2,
+            b.as_ptr(),
+            2,
+            0.0,
+            c.as_mut_ptr(),
+            2,
+        );
+    }
+
+    assert_eq!(c, [2.0, 4.0, 10.0, 14.0]);
+}
---
+++ tests/test_openblas_extensions.rs
@@ -0,0 +1,61 @@
+#![cfg(all(feature = "openblas", not(feature = "intel-mkl")))]
+
+use rivet_blas_sys::cblas::cblas_level_one_openblas::cblas_saxpby;
+use rivet_blas_sys::cblas::cblas_level_three_openblas::{cblas_sgeadd, cblas_somatcopy};
+use rivet_blas_sys::cblas::cblas_types::{CBlasLayout, CBlasTranspose};
+
+#[test]
+fn openblas_saxpby_updates_y_with_two_scalars() {
+    let x = [1.0_f32, 2.0, 3.0];
+    let mut y = [10.0_f32, 20.0, 30.0];
+
+    unsafe {
+        cblas_saxpby(3, 2.0, x.as_ptr(), 1, 0.5, y.as_mut_ptr(), 1);
+    }
+
+    assert_eq!(y, [7.0, 14.0, 21.0]);
+}
+
+#[test]
+fn openblas_somatcopy_copies_a_matrix() {
+    let a = [1.0_f32, 2.0, 3.0, 4.0];
+    let mut b = [0.0_f32; 4];
+
+    unsafe {
+        cblas_somatcopy(
+            CBlasLayout::CBlasRowMajor,
+            CBlasTranspose::CBlasNoTrans,
+            2,
+            2,
+            1.0,
+            a.as_ptr(),
+            2,
+            b.as_mut_ptr(),
+            2,
+        );
+    }
+
+    assert_eq!(b, a);
+}
+
+#[test]
+fn openblas_sgeadd_combines_two_matrices() {
+    let a = [1.0_f32, 2.0, 3.0, 4.0];
+    let mut c = [5.0_f32, 6.0, 7.0, 8.0];
+
+    unsafe {
+        cblas_sgeadd(
+            CBlasLayout::CBlasRowMajor,
+            2,
+            2,
+            2.0,
+            a.as_ptr(),
+            2,
+            3.0,
+            c.as_mut_ptr(),
+            2,
+        );
+    }
+
+    assert_eq!(c, [17.0, 22.0, 27.0, 32.0]);
+}
